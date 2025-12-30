b70eb774b948921be771a4e87967aa1f0a7e47b3
jangmk05 <jangmk05@gmail.com>
2025-12-30 13:21:08 +0900

    [FEAT] 공용 Button 컴포넌트 추가

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface ButtonProps {
+  onClick?: () => void;
+  text: String;
+  type: "button" | "submit" | "reset";
+}
+
+export default function Button({ onClick, text, type }: ButtonProps) {
+  return (
+    <button
+      className="w-[343px] px-[10px] py-[16px] flex items-center justify-center gap-[10px]  bg-[#FF885A] rounded-xl font-sans text-white font-semibold text-[20px] leading-[22px] text-center"
+      onClick={onClick}
+      type={type}
+    >
+      {text}
+    </button>
+  );
+}
