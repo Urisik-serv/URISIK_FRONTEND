548a351498cbe0f44ffbecc6bed864e37753f3c5
jangmk05 <jangmk05@gmail.com>
2026-01-02 11:43:01 +0900

    [FEAT] 공용 BackButton 추가

---
+++ src/components/common/BackButton.tsx
@@ -0,0 +1,31 @@
+import { useNavigate, type To } from "react-router-dom";
+import BackIcon from "../../assets/icons/chevron-left.svg";
+
+interface BackButtonProps {
+  to?: number | To;
+  className?: string;
+}
+
+export default function BackButton({
+  to = -1,
+  className = "",
+}: BackButtonProps) {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    if (typeof to === "number") {
+      navigate(to);
+    } else {
+      navigate(to);
+    }
+  };
+
+  return (
+    <button
+      onClick={handleClick}
+      className={`cursor-pointer size-[24px] ${className}`}
+    >
+      <img src={BackIcon} alt="뒤로가기" />
+    </button>
+  );
+}
