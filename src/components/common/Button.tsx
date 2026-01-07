b09dfea71ae71fe787b9fae117191e957a7b0987
jangmk05 <jangmk05@gmail.com>
2026-01-07 22:10:32 +0900

    [REFACTOR] 공용 Button cursor-pointer 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -7,7 +7,7 @@ interface ButtonProps {
 export default function Button({ onClick, text, type }: ButtonProps) {
   return (
     <button
-      className="w-[343px] px-[10px] py-[16px] flex items-center justify-center gap-[10px]  bg-[#FF885A] rounded-xl font-sans text-white font-semibold text-[20px] leading-[22px] text-center"
+      className="w-[343px] px-[10px] py-[16px] flex items-center justify-center gap-[10px]  bg-[#FF885A] rounded-xl font-sans text-white font-semibold text-[20px] leading-[22px] text-center cursor-pointer"
       onClick={onClick}
       type={type}
     >
