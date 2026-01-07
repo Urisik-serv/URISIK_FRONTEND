f2f17a37cf157b71977b18b10b87cead6d020f35
shooukie <bigcape@naver.com>
2026-01-07 21:04:46 +0900

    [FEAT] navigate연결, 디자인 수정

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -2,36 +2,50 @@ import Background from "../../assets/icons/footer-background.svg";
 import Home from "../../assets/icons/home.svg";
 import Calendar from "../../assets/icons/calendar.svg";
 import Note from "../../assets/icons/note-edit.svg";
+import { useNavigate } from "react-router-dom";
 
 export default function Footer() {
+  const navigate = useNavigate();
+
   return (
     <div className="fixed bottom-0 text-[10px]">
       <div className="relative">
         <img src={Background} className="w-full" alt="푸터 배경" />
 
-        <div className="absolute bottom-0 w-full aspect-[375/92] flex justify-between px-6 ">
-          <div className="flex flex-col items-center gap-1 w-19 pt-2">
-            <img src={Home} className="size-5" alt="루틴 아이콘" />
-            <p>루틴</p>
+        <div className="absolute bottom-0 w-full aspect-[375/92] flex justify-between px-6">
+          <div className="flex justify-center w-19 pt-2">
+            <button
+              className="flex flex-col cursor-pointer gap-1"
+              onClick={() => navigate("/")}
+            >
+              <img src={Home} className="size-5" alt="루틴 아이콘" />
+              <p>루틴</p>
+            </button>
           </div>
 
-          <div className="flex flex-col items-center gap-1 w-19 pt-2">
-            <img src={Calendar} className="size-5" alt="식단표 아이콘" />
-            <p>식단표</p>
+          <div className="flex flex-col items-center w-19 pt-2">
+            <button
+              className="flex flex-col items-center cursor-pointer gap-1"
+              onClick={() => navigate("/planner")}
+            >
+              <img src={Calendar} className="size-5" alt="식단표 아이콘" />
+              <p>식단표</p>
+            </button>
           </div>
         </div>
 
-        <div className="absolute bottom-[clamp(32px,calc(56/375*100vw),56px)] w-full flex justify-center">
-          <div
-            className="flex flex-col justify-center items-center gap-1 
+        <button
+          className="
+            absolute left-1/2 -translate-x-1/2 bottom-[clamp(32px,calc(56/375*100vw),56px)]
+            flex flex-col justify-center items-center gap-1 
             w-[clamp(40px,calc(68/375*100vw),68px)]
             h-[clamp(40px,calc(68/375*100vw),68px)] 
-            rounded-full bg-[#FF885A]"
-          >
-            <img src={Note} className="size-5" alt="식단짜기 아이콘" />
-            <p className="text-white">식단 짜기</p>
-          </div>
-        </div>
+            rounded-full bg-[#FF885A] cursor-pointer"
+          onClick={() => navigate("/meal-plan")}
+        >
+          <img src={Note} className="size-5" alt="식단짜기 아이콘" />
+          <p className="text-white">식단 짜기</p>
+        </button>
       </div>
     </div>
   );
---
+++ src/pages/planner/planner-page.tsx
@@ -0,0 +1,5 @@
+const PlannerPage = () => {
+  return <div>식단표페이지 입니다.</div>;
+};
+
+export default PlannerPage;
