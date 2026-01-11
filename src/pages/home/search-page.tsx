24cc4ed8a3124089c3f742fbb2aecb0c548e0e36
tablemin03 <vs201466@naver.com>
2026-01-11 16:18:07 +0900

    [DESIGN] 검색창 위치 변경

--- src/pages/home/search-page.tsx
+++ src/pages/home/search-page.tsx
@@ -3,7 +3,7 @@ import SearchBar from "../../components/common/SearchBar";
 
 const SearchingPage = () => {
   return (
-    <div className="flex px-4 py-2 justify-center items-center ">
+    <div className="flex pt-[30px] px-4 py-2 justify-center items-center ">
       <BackButton />
       <SearchBar />
     </div>
