28714fc28e5fafc0be0bfb660ead8eb1cc25ca54
tablemin03 <vs201466@naver.com>
2026-01-11 17:23:21 +0900

    [FEAT] 인기 메뉴 Card 컴포넌트 구현

---
+++ src/components/home/curation/AllergyCuration.tsx
@@ -0,0 +1,19 @@
+import AllergyCard from "./AllergyCard";
+
+const AllergyCuration = () => {
+  return (
+    <div>
+      <div className="pb-4">
+        <h2 className="pt-[31px] text-zinc-800 text-xl font-semibold tracking-tight">
+          같은 알레르기 가족에게 인기 메뉴
+        </h2>
+        <p className="text-neutral-400 text-sm font-medium leading-6">
+          같은 알레르기를 소유한 가족원들 사이에서 인기가 많아요.
+        </p>
+      </div>
+      <AllergyCard />
+    </div>
+  );
+};
+
+export default AllergyCuration;
