c4ab2b5be355c25bed3227656a0188d8640d972d
shooukie <bigcape@naver.com>
2026-01-14 01:18:09 +0900

    [FEAT] 식단 생성 페이지 UI 구현

--- src/pages/meal-plan/meal-plan-page.tsx
+++ src/pages/meal-plan/meal-plan-page.tsx
@@ -1,5 +1,28 @@
+import Button from "../../components/common/Button";
+import HomeHeader from "../../components/header/HomeHeader";
+import ChooseWeek from "../../components/meal-plan/chooseWeek";
+
 const MealPlanPage = () => {
-  return <div>식단짜기페이지 입니다.</div>;
+  return (
+    <div className="">
+      <HomeHeader />
+      <div className="px-4 pt-6">
+        <p className="font-semibold text-[24px] text-[#333333] pb-2">
+          요일별 식사 횟수를 선택해요.
+        </p>
+        <p className="font-medium text-[16px] text-[#929292] pb-11 whitespace-pre-line">
+          가족원의 위시리스트를 기반으로 {"\n"} 다음주 식단을 생성돼요.
+        </p>
+        <div className="flex flex-col gap-5">
+          <ChooseWeek mealTime="점심" />
+          <ChooseWeek mealTime="저녁" />
+        </div>
+        <div className="fixed bottom-11">
+          <Button text="식단 생성" type="button" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MealPlanPage;
