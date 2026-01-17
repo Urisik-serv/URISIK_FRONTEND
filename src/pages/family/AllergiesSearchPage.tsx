f6b158d252b97d9896949fa894a1648217521a40
jangmk05 <jangmk05@gmail.com>
2026-01-18 00:50:52 +0900

    [BUGFIX] 충돌 해결

---
+++ src/pages/family/AllergiesSearchPage.tsx
@@ -0,0 +1,9 @@
+import PublicHeader from "../../components/header/PublicHeader";
+
+export default function AllergiesSearchPage() {
+  return (
+    <>
+      <PublicHeader title={""} />
+    </>
+  );
+}
