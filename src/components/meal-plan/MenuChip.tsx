fa77ff4e0fdeee9c182d061499bf122246e947e5
shooukie <bigcape@naver.com>
2026-01-18 01:53:47 +0900

    [FEAT] menuChip 컴포넌트 선택 기능 추가

--- src/components/meal-plan/MenuChip.tsx
+++ src/components/meal-plan/MenuChip.tsx
@@ -1,6 +1,21 @@
-export default function MenuChip() {
+import { useState } from "react";
+type MenuChipProps = {
+  clickable?: boolean;
+};
+
+
+export default function MenuChip({clickable=false}:MenuChipProps) {
+  const [isClick, setIsClick]=useState(false);
+
+  const handleClick=()=>{
+    setIsClick((prev)=>!prev);
+  }
+  
   return (
-    <div className="w-[66px] h-[62px] bg-[#f6f6f6] rounded-xl flex justify-center items-center text-center font-medium text-[14px] whitespace-pre-line">
+    <div className={`w-[66px] h-[62px] rounded-xl flex justify-center items-center text-center font-medium text-[14px] whitespace-pre-line 
+    ${isClick ? "bg-primary-100 border border-[1.5px] border-primary-700 cursor-pointer" : "bg-[#f6f6f6]"}`}
+    onClick={clickable ? handleClick : undefined}
+    >
       쌀가루 {"\n"}피자
     </div>
   );
