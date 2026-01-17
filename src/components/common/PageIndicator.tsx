366c93bfe29f76412054784c9101d178464c9b8c
tablemin03 <vs201466@naver.com>
2026-01-17 18:34:34 +0900

    [FEAT] 페이지 인디케이터 누르면 해당 페이지로 이동 효과 추가

--- src/components/common/PageIndicator.tsx
+++ src/components/common/PageIndicator.tsx
@@ -1,9 +1,10 @@
 interface PageIndicatorProps {
   page: number;
   total: number;
+  onClick?: (e: number) => void;
 }
 
-const PageIndicator = ({ page, total }: PageIndicatorProps) => {
+const PageIndicator = ({ page, total, onClick }: PageIndicatorProps) => {
   return (
     <div className="inline-flex justify-start items-center gap-[3px]">
       {Array.from({ length: total }).map((_, index) => {
@@ -11,8 +12,9 @@ const PageIndicator = ({ page, total }: PageIndicatorProps) => {
         return (
           <div
             key={index}
+            onClick={() => onClick?.(index + 1)}
             className={`
-              h-1.5 rounded-[3px] transition-all duration-300 ease-in-out
+              h-1.5 rounded-[3px] transition-all duration-300 ease-in-out cursor-pointer
               ${isFocused ? "w-6 bg-primary-700" : "w-1.5 bg-neutral-200"}`}
           />
         );
