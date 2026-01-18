72ed42d061fafda805a938f4cea7b1af9d64ae11
jangmk05 <jangmk05@gmail.com>
2026-01-19 01:02:43 +0900

    [REFACTOR] 폼 입력 컴포넌트로 변경

--- src/pages/family/modify-profile-page.tsx
+++ src/pages/family/modify-profile-page.tsx
@@ -1,3 +1,13 @@
+import ProfileDataForm from "../../components/family/ProfileDataForm";
+import PublicHeader from "../../components/header/PublicHeader";
+
 export default function ModifyProfilePage() {
-  return <div>ModifyProfilePage</div>;
+  return (
+    <>
+      <PublicHeader title={"프로필 편집"} />
+      <div className="flex justify-center">
+        <ProfileDataForm isSelected={0} isEdit={true} />
+      </div>
+    </>
+  );
 }
