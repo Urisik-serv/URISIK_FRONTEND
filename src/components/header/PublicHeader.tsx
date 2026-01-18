d04f480ae0f635f7c8ae115b30f7170985fc0173
jangmk05 <jangmk05@gmail.com>
2026-01-18 23:27:31 +0900

    [REFACTOR] to(경로) props 추가

--- src/components/header/PublicHeader.tsx
+++ src/components/header/PublicHeader.tsx
@@ -1,13 +1,15 @@
+import type { To } from "react-router-dom";
 import BackButton from "../common/BackButton";
 
 interface PublicHeaderProps {
   title: string | null;
+  to?: number | To;
 }
 
-const PublicHeader = ({ title }: PublicHeaderProps) => {
+const PublicHeader = ({ title, to }: PublicHeaderProps) => {
   return (
     <div className="w-full px-4 py-2.5 flex justify-between items-center">
-      <BackButton />
+      <BackButton to={to} />
       <div className="">
         <p className="justify-start text-black text-xl font-semibold font-['Pretendard'] tracking-tight">
           {title}
