5987318a181607d0d444df0aa94ab77e61621c21
jangmk05 <jangmk05@gmail.com>
2026-01-20 01:18:16 +0900

    [FEAT] 위시리스트 박스 컴포넌트 분리

---
+++ src/components/test/WishListBox.tsx
@@ -0,0 +1,9 @@
+export default function WishListBox() {
+  return (
+    <div className="w-[343px] h-[72px]">
+      <div className="p-[10px]">
+        <div></div>
+      </div>
+    </div>
+  );
+}
