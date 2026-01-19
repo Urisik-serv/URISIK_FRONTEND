6e17cf89f25b1baecedd4334164e2a5604e92171
tablemin03 <vs201466@naver.com>
2026-01-19 15:31:26 +0900

    [FEAT] 프로필 카드 스타일 수정 및 onClick 효과 추가

--- src/components/home/profile/FamilyProfile.tsx
+++ src/components/home/profile/FamilyProfile.tsx
@@ -3,13 +3,14 @@ import HomeProfileCard from "./HomeProfileCard";
 
 const FamilyProfile = () => {
   const navigate = useNavigate();
+
   return (
     <div className="w-full  px-4 pt-5 pb-3 bg-white rounded-xl outline-1 outline-stone-300 flex flex-col justify-start items-center gap-3  ">
       <p className="self-stretch justify-start text-black text-lg font-medium leading-7">
         우리가족 프로필
       </p>
       <div className="flex self-stretch items-center gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden [scrollbar-width:none]">
-        <HomeProfileCard name="내 프로필" />
+        <HomeProfileCard onClick={() => navigate("mypage")} name="내 프로필" />
         <HomeProfileCard name="강아빠" />
         <HomeProfileCard name="강민지" />
       </div>
--- src/components/home/profile/HomeProfileCard.tsx
+++ src/components/home/profile/HomeProfileCard.tsx
@@ -1,17 +1,20 @@
 import DefaultMomImg from "../../../assets/profile/default-mom.svg";
 interface HomeProfileCardProps {
   name: string;
+  onClick?: () => void;
   // myProfile: boolean;
 }
 
-const HomeProfileCard = ({ name }: HomeProfileCardProps) => {
+const HomeProfileCard = ({ name, onClick }: HomeProfileCardProps) => {
+  const myProfile = name === "내 프로필";
   return (
     <div className="shrink-0">
       <img
         src={DefaultMomImg}
         alt="프로필 이미지"
-        className={`w-14 h-14 rounded-full ${
-          name === "내 프로필" ? "border-[3px] border-primary-700" : ""
+        onClick={onClick}
+        className={`w-14 h-14 rounded-full cursor-pointer ${
+          myProfile ? "border-[3px] border-primary-700" : ""
         }`}
       />
       <p className="text-center text-neutral-500 text-sm font-normal leading-5">
