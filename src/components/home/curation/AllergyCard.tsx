cec8e972132ba1e936fed5042af327a97251d211
tablemin03 <vs201466@naver.com>
2026-01-20 20:18:33 +0900

    [FEAT] mock data fetching 후 알레르기 카드에 데이터 연결

--- src/components/home/curation/AllergyCard.tsx
+++ src/components/home/curation/AllergyCard.tsx
@@ -1,23 +1,35 @@
 import SampleImg from "../../../assets/sample/shrimp-mushroom.png";
 import HeartFull from "../../../assets/icons/heart-full.svg";
 
-const AllergyCard = () => {
+interface AllergyCardProps {
+  id: number;
+  title: string;
+  shortDescription: string;
+  pickedCount: number;
+}
+
+const AllergyCard = ({
+  id,
+  title,
+  shortDescription,
+  pickedCount,
+}: AllergyCardProps) => {
   return (
     <div className="py-3 px-2.5 rounded-xl border-2 border-[#ECECEC]">
       <div className="flex justify-between items-center h-32">
         <div className="w-44">
           <div className="pb-4">
             <h1 className="text-zinc-800 text-[15px] font-semibold leading-6 pb-2 cursor-pointer">
-              새우 알레르기 가족원 사이에서 인기있는 버섯 두부 새우탕!
+              {title}
             </h1>
             <p className="text-neutral-500 text-xs font-medium leading-5 cursor-pointer">
-              표고 버섯과 두부피로 새우의 식감을 살린 메뉴로 주목받고 있어요.
+              {shortDescription}
             </p>
           </div>
           <div className="flex justify-start gap-0.5">
             <img src={HeartFull} alt="좋아요" />
             <p className="text-neutral-400 text-xs font-semibold leading-4">
-              127가구가 선택했어요
+              {pickedCount}가구가 선택했어요
             </p>
           </div>
         </div>
--- src/components/home/curation/AllergyCuration.tsx
+++ src/components/home/curation/AllergyCuration.tsx
@@ -1,11 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PageIndicator from "../../common/PageIndicator";
 import AllergyCard from "./AllergyCard";
 import { Swiper as SwiperType } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
+import type { FoodList } from "../../../types/recipe-list";
+import axios from "axios";
 
 const AllergyCuration = () => {
+  // 슬라이드 효과
   const [activePage, setActivePage] = useState(1);
   const [swiperRef, setSwiperRef] = useState<SwiperType | null>(null);
   const handlePage = (pageNum: number) => {
@@ -14,6 +17,22 @@ const AllergyCuration = () => {
     }
     setActivePage(pageNum);
   };
+
+  // mock data fetching
+  const [data, setData] = useState<FoodList | null>(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("/data/recipe-list.json");
+
+        setData(response.data);
+      } catch (error) {
+        console.log("데이터 로딩 실패: ", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div>
@@ -25,26 +44,30 @@ const AllergyCuration = () => {
           같은 알레르기를 소유한 가족원들 사이에서 인기가 많아요.
         </p>
       </div>
-      <Swiper
-        modules={[Autoplay]}
-        spaceBetween={16}
-        slidesPerView={1}
-        loop={true}
-        autoplay={{ delay: 5000, disableOnInteraction: false }}
-        onSwiper={(swiper) => setSwiperRef(swiper)}
-        onSlideChange={(swiper) => setActivePage(swiper.realIndex + 1)}
-        className="pb-2"
-      >
-        <SwiperSlide>
-          <AllergyCard />
-        </SwiperSlide>
-        <SwiperSlide>
-          <AllergyCard />
-        </SwiperSlide>
-        <SwiperSlide>
-          <AllergyCard />
-        </SwiperSlide>
-      </Swiper>
+      {data && (
+        <Swiper
+          modules={[Autoplay]}
+          spaceBetween={16}
+          slidesPerView={1}
+          loop={true}
+          autoplay={{ delay: 5000, disableOnInteraction: false }}
+          onSwiper={(swiper) => setSwiperRef(swiper)}
+          onSlideChange={(swiper) => setActivePage(swiper.realIndex + 1)}
+          className="pb-2"
+        >
+          {data?.recipes.map((recipe) => (
+            <SwiperSlide>
+              <AllergyCard
+                key={recipe.id}
+                id={recipe.id}
+                title={recipe.title}
+                shortDescription={recipe.shortDescription}
+                pickedCount={recipe.pickedCount}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      )}
       <div className="flex justify-center pt-2">
         <PageIndicator page={activePage} total={3} onClick={handlePage} />
       </div>
