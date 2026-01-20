3528def19e25e0ac2c909a980cc4d84607c082f5
tablemin03 <vs201466@naver.com>
2026-01-20 19:53:41 +0900

    [CHORE] 음식 mock data 추가 및 타입 선언

---
+++ src/types/recipe-list.ts
@@ -0,0 +1,29 @@
+export interface Recipe {
+  id: number;
+  title: string;
+  category: string;
+  tags: string[];
+  shortDescription: string;
+  rating: number;
+  pickedCount: number;
+  imageUrl: string;
+  meta: {
+    cookingTime: string;
+    difficulty: {
+      level: number;
+      label: string;
+    };
+    tastePoint: string;
+  };
+  substitutionLogics: [
+    {
+      title: string;
+      descriptions: string[];
+    },
+  ];
+  ingredients: string[];
+  isWishlisted: boolean;
+}
+export interface FoodList {
+  racipes: Recipe[];
+}
