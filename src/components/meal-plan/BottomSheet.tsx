97a92e103cdfa40ace1303ec56c5de977553077d
shooukie <bigcape@naver.com>
2026-01-20 17:55:39 +0900

    [FEAT] 식단 수정하기 UI구현

---
+++ src/components/meal-plan/BottomSheet.tsx
@@ -0,0 +1,91 @@
+import Button from "../common/Button";
+import Up from "../../assets/icons/chevron-up.svg";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import MenuList from "../common/MenuList";
+
+type BottomSheetProps = {
+  open: boolean;
+};
+export default function BottomSheet({ open }: BottomSheetProps) {
+  const [tab, setTab] = useState<"common" | "recommend">("common");
+  const [isOpen, setIsOpen] = useState(false);
+  const [selected, setSelected] = useState(0);
+  const navigate = useNavigate();
+  const menus = [
+    "바나나 프렌치토스트",
+    "바나나 프렌치토스트",
+    "바나나 프렌치토스트",
+    "바나나 프렌치토스트",
+    "바나나 프렌치토스트",
+    "바나나 프렌치토스트",
+    "바나나 프렌치토스트",
+    "바나나 프렌치토스트",
+    "바나나 프렌치토스트",
+  ]; //임시데이터
+  useEffect(() => {
+    setIsOpen(open);
+  }, [open]);
+  return (
+    <div className="w-[375px] fixed bottom-0 p-[10px] rounded-t-2xl shadow-[0_-2px_4px_-2px_rgba(0,0,0,0.25)] overflow-hidden bg-white flex flex-col items-center">
+      <div className="">
+        <button
+          className="flex items-center justify-center size-8 mb-3 cursor-pointer"
+          onClick={() => {
+            setIsOpen((prev) => !prev);
+          }}
+        >
+          <img src={Up} alt="위로 올리기" />
+        </button>
+      </div>
+      <div className="w-full flex font-semibold text-[18px] h-13">
+        <button
+          className={`flex-1 cursor-pointer ${tab === "common" ? "bg-gray-50 text-primary-700 " : "bg-white text-gray-300"} ${tab === "common" && isOpen ? "border-b-2 border-primary-700" : ""}`}
+          onClick={() => setTab("common")}
+        >
+          공통
+        </button>
+        <button
+          className={`flex-1 cursor-pointer ${tab === "recommend" ? "bg-gray-50 text-primary-700" : "bg-white text-gray-300"} ${tab === "recommend" && isOpen ? "border-b-2 border-primary-700" : ""}`}
+          onClick={() => setTab("recommend")}
+        >
+          추천
+        </button>
+      </div>
+
+      <div
+        className={`w-full overflow-auto transition-[max-height,opacity,transform] duration-300 ${isOpen ? "max-h-[475px] opacity-100 translate-y-0" : "max-h-0 opacity-0 translate-y-4"}`}
+      >
+        <div className="py-4">
+          {menus.map((menu, idx) => {
+            const isMenuListSelect = selected === idx;
+            return (
+              <MenuList
+                key={idx}
+                type="profile"
+                menu={menu}
+                clickable={true}
+                isSelected={isMenuListSelect}
+                onClick={() => setSelected(idx)}
+              />
+            );
+          })}
+        </div>
+      </div>
+
+      <div className="p-[10px]">
+        <Button
+          text={isOpen ? "바꾸기" : "수정완료"}
+          type="button"
+          onClick={() => {
+            if (isOpen) {
+              setIsOpen(false);
+            } else {
+              navigate("/");
+            }
+          }}
+        />
+      </div>
+    </div>
+  );
+}
--- src/pages/meal-plan/meal-plan-edit-page.tsx
+++ src/pages/meal-plan/meal-plan-edit-page.tsx
@@ -1,8 +1,76 @@
+import { useEffect, useRef, useState } from "react";
+import PublicHeader from "../../components/header/PublicHeader";
+import CalendarChipM from "../../components/meal-plan/CalendarChip/CalendarChipM";
+import MenuChip from "../../components/meal-plan/MenuChip";
+import BottomSheet from "../../components/meal-plan/BottomSheet";
 
 const MealPlanEditPage = () => {
+  const data = [
+    { day: "월", menus: ["바나나", "김밥"] },
+    { day: "화", menus: ["귤", "김밥", "김밥"] },
+    { day: "수", menus: ["귤", "김밥", "김밥"] },
+    { day: "목", menus: ["귤"] },
+    { day: "금", menus: ["김밥"] },
+    { day: "토", menus: ["귤", "김밥", "김밥"] },
+    { day: "일", menus: ["귤", "김밥"] },
+  ]; //임시 데이터
+  const [selected, setSelected] = useState<{
+    dayIndex: number | null;
+    menuIndex: number | null;
+  }>({ dayIndex: null, menuIndex: null });
+  const [open, setOpen] = useState(false);
+
+  const hasOpenedRef = useRef(false);
+  useEffect(() => {
+    if (selected.menuIndex !== null && !hasOpenedRef.current) {
+      hasOpenedRef.current = true;
+      const timer = setTimeout(() => {
+        setOpen(true);
+      }, 1500);
+      return () => clearTimeout(timer);
+    }
+  }, [selected]);
+
   return (
     <div>
-        식단 수정하기 페이지 입니다
+      <PublicHeader title={""} />
+      <p className="pl-4 pt-6 font-semibold text-[24px] text-[#333333] pb-4 whitespace-pre-line">
+        수정하고 싶은 요일을 선택하고,{"\n"}내가 원하는 메뉴로 바꿔요.
+      </p>
+      <div className="flex gap-2 pt-2 px-4 overflow-x-auto">
+        {data.map((data, dayIndex) => {
+          const isDaySelected = selected.dayIndex === dayIndex;
+          return (
+            <div className="flex flex-col items-center gap-3">
+              <CalendarChipM text={data.day} isSelect={isDaySelected} />
+              {data.menus.map((menu, menuIndex) => {
+                const isMenuSelected =
+                  selected.menuIndex === menuIndex &&
+                  selected.dayIndex === dayIndex;
+                return (
+                  <MenuChip
+                    text={menu}
+                    clickable={true}
+                    isSelect={isMenuSelected}
+                    onClick={() => {
+                      const isAlreadySelected =
+                        selected.menuIndex === menuIndex &&
+                        selected.dayIndex === dayIndex;
+                      if (isAlreadySelected) {
+                        //선택된 메뉴를 다시 누르는 경우
+                        setSelected({ dayIndex: null, menuIndex: null });
+                      } else {
+                        setSelected({ dayIndex, menuIndex });
+                      }
+                    }}
+                  />
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+      <BottomSheet open={open} />
     </div>
   );
 };
