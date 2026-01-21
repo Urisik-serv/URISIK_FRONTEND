19ac84f41348bc9b18c39872432904db095919fb
tablemin03 <vs201466@naver.com>
2026-01-21 17:04:32 +0900

    [REFACTOR] prams 이름 변경

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -68,7 +68,7 @@ export const router = createBrowserRouter([
             element: <AllergiesSearchPage />,
           },
           {
-            path: "menu-information/:menuid",
+            path: "menu-information/:menuId",
             element: <MenuInformationPage />,
           },
         ],
