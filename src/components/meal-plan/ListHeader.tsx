8b3c3bac8550db59a3832377b47e51da3839162a
shooukie <bigcape@naver.com>
2026-01-21 17:49:29 +0900

    [FEAT] 식재료 바구니 UI 구현

---
+++ src/components/meal-plan/ListHeader.tsx
@@ -0,0 +1,38 @@
+import Up from "../../assets/icons/chevron-up-gray.svg";
+import Down from "../../assets/icons/chevron-down-gray.svg";
+
+type ListHeaderProps = {
+  toggleable?: boolean;
+  title: string;
+  date: string;
+  isOpen?: boolean;
+  setIsOpen?: () => void;
+};
+
+export default function ListHeader({
+  toggleable = false,
+  title,
+  date,
+  isOpen = true,
+  setIsOpen,
+}: ListHeaderProps) {
+  const buttonSrc = isOpen ? Up : Down;
+  return (
+    <div
+      className={`flex px-[10px] py-1 rounded-lg text-4 gap-2 ${
+        isOpen ? "bg-secondary-700" : "bg-gray-200"
+      }`}
+    >
+      {toggleable && (
+        <button
+          className="size-6 flex justify-center items-center cursor-pointer"
+          onClick={setIsOpen}
+        >
+          <img src={buttonSrc} alt="열고 닫기" />
+        </button>
+      )}
+      <p className="font-semibold">{title}</p>
+      <p className="font-medium text-gray-600">{date}</p>
+    </div>
+  );
+}
