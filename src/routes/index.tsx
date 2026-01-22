c1e08227fb6e599b3f3f7f7f644b2b747efcf777
jangmk05 <jangmk05@gmail.com>
2026-01-23 01:05:49 +0900

    [FEAT] 로그아웃 & 가족 계정 초기 페이지 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -26,6 +26,8 @@ import ChildrenAndFamily from "../pages/terms-and-policies/children-and-family";
 import MarketingPreferences from "../pages/terms-and-policies/marketing-preferences";
 import PrivacyPolicy from "../pages/terms-and-policies/privacy-policy";
 import TermsOfService from "../pages/terms-and-policies/term-of-service";
+import FamilyAccount from "../pages/mypage/family-account";
+import LogOut from "../pages/mypage/log-out";
 
 export const router = createBrowserRouter([
   {
@@ -63,6 +65,8 @@ export const router = createBrowserRouter([
           },
           { path: "mypage", element: <MyPage /> },
           { path: "mypage/terms-and-policies", element: <TermsAndPolicies /> }, // 마이페이지 라우팅, path는 임시로 부여
+          { path: "mypage/family-account", element: <FamilyAccount /> },
+          { path: "mypage/log-out", element: <LogOut /> },
           { path: "planner", element: <PlannerPage /> },
           { path: "meal-plan/create", element: <MealPlanCreatePage /> },
           { path: "meal-plan/result", element: <MealPlanResultPage /> },
