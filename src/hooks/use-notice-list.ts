ee922caa9189c5c2220adca647026f914f40641a
jangmk05 <jangmk05@gmail.com>
2026-01-24 18:31:54 +0900

    [FEAT] 데이터 불러오는 훅 추가

---
+++ src/hooks/use-notice-list.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import type { NoticeLists } from "../types/notice-list";
+import axios from "axios";
+
+export const useNoticeList = () => {
+  const [noticeList, setNoticeList] = useState<NoticeLists | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get<NoticeLists>(
+          "/public/data/notice-list.json",
+        );
+
+        setNoticeList(res.data);
+        console.log(res);
+      } catch (error) {
+        console.log("데이터 로딩 실패:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return { noticeList };
+};
