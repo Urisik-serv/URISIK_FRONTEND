a5876e95f35ab89d4b8443a7785b9029647fe42a
tablemin03 <vs201466@naver.com>
2026-01-25 01:04:37 +0900

    [BUGFIX] 돋보기 아이콘이 input 박스 밖으로 나가는 현상 처리

--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -6,12 +6,12 @@ const SearchBar = () => {
       <form className="self-stretch h-11 pl-4 pr-3 py-2 bg-gray-50 rounded-[999px] outline-1 outline-stone-200 flex-1 flex justify-between items-center text-gray-600">
         <input
           placeholder="조리를 원하시는 음식을 입력해주세요."
-          className="justify-start flex-1 text-sm font-medium leading-6 focus:outline-none"
+          className="justify-start flex-1 text-sm font-medium leading-6 focus:outline-none min-w-0"
         />
         <img
           src={SearchIcon}
           alt="돋보기 아이콘"
-          className="cursor-pointer border-gray-600"
+          className="w-7 h-7 shrink-0 cursor-pointer"
         />
       </form>
     </>
