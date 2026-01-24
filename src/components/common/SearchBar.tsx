6d6010d8377dace7c3625afe54d8a9507da3b9b8
tablemin03 <vs201466@naver.com>
2026-01-25 00:55:06 +0900

    [CHORE] search-icon 교체

--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -3,12 +3,16 @@ import SearchIcon from "../../assets/icons/search-icon.svg";
 const SearchBar = () => {
   return (
     <>
-      <form className="self-stretch h-11 pl-4 pr-3 py-2 bg-stone-100 rounded-[999px] outline-1 outline-stone-200 flex-1 flex justify-between items-center">
+      <form className="self-stretch h-11 pl-4 pr-3 py-2 bg-gray-50 rounded-[999px] outline-1 outline-stone-200 flex-1 flex justify-between items-center text-gray-600">
         <input
           placeholder="조리를 원하시는 음식을 입력해주세요."
           className="justify-start flex-1 text-sm font-medium leading-6 focus:outline-none"
         />
-        <img src={SearchIcon} alt="돋보기 아이콘" className="cursor-pointer" />
+        <img
+          src={SearchIcon}
+          alt="돋보기 아이콘"
+          className="cursor-pointer border-gray-600"
+        />
       </form>
     </>
   );
