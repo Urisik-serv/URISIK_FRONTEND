4f400c467ff4e631253545d698f52eb12a2b3843
shooukie <bigcape@naver.com>
2026-01-24 23:27:31 +0900

    [FEAT] CalendarChipS 타입 추가 및 변경 적용

--- src/components/meal-plan/ChooseWeek.tsx
+++ src/components/meal-plan/ChooseWeek.tsx
@@ -12,7 +12,7 @@ export default function ChooseWeek({ mealTime }: ChooseWeekProps) {
       <p className="text-[18px] font-medium">{mealTime} 식사</p>
       <div className="flex gap-3 pt-3">
         {week.map((day) => (
-          <CalenderChip key={day} text={day} />
+          <CalenderChip key={day} text={day} type="select" />
         ))}
       </div>
     </div>
