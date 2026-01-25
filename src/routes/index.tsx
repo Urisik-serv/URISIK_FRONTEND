61f433fda17bd97ac3b6624d8c6292dc704f49d3
jangmk05 <jangmk05@gmail.com>
2026-01-25 11:06:40 +0900

    [FEAT] 알림 페이지 라우트 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -26,6 +26,7 @@ import ChildrenAndFamily from "../pages/terms-and-policies/children-and-family";
 import MarketingPreferences from "../pages/terms-and-policies/marketing-preferences";
 import PrivacyPolicy from "../pages/terms-and-policies/privacy-policy";
 import TermsOfService from "../pages/terms-and-policies/term-of-service";
+import NoticePage from "../pages/mypage/notice-page";
 
 export const router = createBrowserRouter([
   {
@@ -63,6 +64,7 @@ export const router = createBrowserRouter([
           },
           { path: "mypage", element: <MyPage /> },
           { path: "mypage/terms-and-policies", element: <TermsAndPolicies /> }, // 마이페이지 라우팅, path는 임시로 부여
+          { path: "mypage/notice", element: <NoticePage /> },
           { path: "planner", element: <PlannerPage /> },
           { path: "meal-plan/create", element: <MealPlanCreatePage /> },
           { path: "meal-plan/result", element: <MealPlanResultPage /> },
