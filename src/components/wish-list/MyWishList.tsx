72f2c4d090893d004727461a1d36b9bab9fc6d5e
tablemin03 <vs201466@naver.com>
2026-01-25 17:58:47 +0900

    [FEAT] 나의 위시리스트 구현

---
+++ src/components/wish-list/MyWishList.tsx
@@ -0,0 +1,93 @@
+import { useState } from "react";
+import EditButton from "../common/EditButton";
+import MenuList from "../common/MenuList";
+import EmptyBox from "../../assets/icons/check-box-empty.svg";
+import CheckedBox from "../../assets/icons/Check_box.svg";
+
+interface Item {
+  id: number;
+  name: string;
+}
+
+const MyWishList = () => {
+  const [items, setItems] = useState<Item[]>([
+    { id: 1, name: "바나나 프렌치토스트" },
+    { id: 2, name: "바나나 프렌치토스트" },
+    { id: 3, name: "바나나 프렌치토스트" },
+    { id: 4, name: "바나나 프렌치토스트" },
+    { id: 5, name: "바나나 프렌치토스트" },
+    { id: 6, name: "바나나 프렌치토스트" },
+    { id: 7, name: "바나나 프렌치토스트" },
+  ]);
+  const [editMode, setEditMode] = useState(false);
+  const [selectedIds, setSelectedIds] = useState<number[]>([]);
+  const toggleSelectItem = (id: number) => {
+    if (!editMode) return;
+
+    setSelectedIds((prev) => {
+      if (prev.includes(id)) {
+        // 이미 선택되어 있다면 제거
+        return prev.filter((itemId) => itemId !== id);
+      } else {
+        // 선택 안 되어 있다면 추가
+        return [...prev, id];
+      }
+    });
+  };
+
+  const handleButtonClick = () => {
+    if (!editMode) {
+      setEditMode(true);
+    } else {
+      if (selectedIds.length > 0) {
+        // 선택된 ID를 제외한 나머지 아이템만 남김
+        const newItems = items.filter((item) => !selectedIds.includes(item.id));
+        setItems(newItems);
+        alert(`${selectedIds.length}개의 아이템이 삭제되었습니다.`);
+      }
+
+      // 초기화 및 편집 모드 OFF
+      setSelectedIds([]);
+      setEditMode(false);
+    }
+  };
+  return (
+    <div>
+      <div className="flex justify-end pt-4">
+        <EditButton
+          onClick={handleButtonClick}
+          count={selectedIds.length}
+          isEditMode={editMode}
+        />
+      </div>
+      <div className="pt-2">
+        {items.map((item) => {
+          const isSelected = selectedIds.includes(item.id);
+          return (
+            <div className="flex items-center">
+              {editMode && (
+                <img
+                  className="cursor-pointer w-6 h-6 shrink-0"
+                  src={`${isSelected ? CheckedBox : EmptyBox}`}
+                  alt={`${isSelected ? "선택됨" : "선택되지 않음"}`}
+                  onClick={() => {
+                    toggleSelectItem(item.id);
+                  }}
+                />
+              )}
+              <MenuList
+                key={item.id}
+                type="default"
+                menu={item.name}
+                clickable={true}
+                isSelected={false}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default MyWishList;
