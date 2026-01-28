c0e6cc945c89b07a08d640da45b50c5f81d63daa
jangmk05 <jangmk05@gmail.com>
2026-01-28 16:42:58 +0900

    [STYLE] 달력 아이콘 가운데 정렬

--- src/components/mypage/DateBlock.tsx
+++ src/components/mypage/DateBlock.tsx
@@ -6,7 +6,7 @@ interface DateProps {
 
 export default function DateBlock({ date }: DateProps) {
   return (
-    <div className="w-[153.5px] px-[16px] py-[12px] border border-gray-300 bg-gray-50 rounded-lg flex justify-start gap-[12px]">
+    <div className="w-[153.5px] px-[16px] py-[12px] border border-gray-300 bg-gray-50 rounded-lg flex justify-start items-center gap-[12px]">
       <div className="size-[18px] flex justify-center items-center">
         <img src={blackCalender} alt="달력" />
       </div>
