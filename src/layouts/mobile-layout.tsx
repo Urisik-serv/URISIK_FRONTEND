592c72150b3a12a449845e4d2edddb5ffc24df81
tablemin03 <vs201466@naver.com>
2026-01-28 14:10:15 +0900

    [FEAT] 사파리에서 검색창이나 위의 컨탠츠가 가려지지 않도록

--- src/layouts/mobile-layout.tsx
+++ src/layouts/mobile-layout.tsx
@@ -8,7 +8,7 @@ export default function MobileLayout() {
 
   const showFooter = footerPaths.includes(location.pathname);
   return (
-    <div className="flex min-h-screen justify-center bg-gray-50">
+    <div className="flex min-h-[100dvh] justify-center bg-gray-50">
       <div className="relative w-full max-w-[375px] bg-white shadow-md">
         <Outlet />
         {showFooter && <Footer />}
