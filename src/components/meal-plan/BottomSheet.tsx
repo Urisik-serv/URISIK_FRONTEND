d282e2cf24df191906f3e862c80b10db18bf0e6f
shooukie <bigcape@naver.com>
2026-01-31 02:11:55 +0900

    [FEAT] 식단표 수정 모달 추가

--- src/components/meal-plan/BottomSheet.tsx
+++ src/components/meal-plan/BottomSheet.tsx
@@ -3,6 +3,7 @@ import Up from "../../assets/icons/chevron-up.svg";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import MenuList from "../common/MenuList";
+import AlertModal from "../common/AlertModal";
 
 type BottomSheetProps = {
   open: boolean;
@@ -10,6 +11,7 @@ type BottomSheetProps = {
 export default function BottomSheet({ open }: BottomSheetProps) {
   const [tab, setTab] = useState<"common" | "recommend">("common");
   const [isOpen, setIsOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [selected, setSelected] = useState(0);
   const navigate = useNavigate();
   const menus = [
@@ -29,6 +31,20 @@ export default function BottomSheet({ open }: BottomSheetProps) {
   return (
     <div className="w-[375px] fixed bottom-0 p-[10px] rounded-t-2xl shadow-[0_-2px_4px_-2px_rgba(0,0,0,0.25)] overflow-hidden bg-white flex flex-col items-center">
       <div>
+        {isModalOpen && (
+          <AlertModal
+            title=""
+            boldContent={`[다음주 식단]에 \n저장되었어요.`}
+            mediumContent="식단표로 이동하시겠습니까?"
+            buttonText="확인"
+            outsideText="탭해서 닫기"
+            onButtonClick={() => navigate(`/meal-plan`)}
+            onOutsideClick={() => {
+              setIsModalOpen(false);
+            }}
+          />
+        )}
+
         <button
           className="flex items-center justify-center size-8 mb-3 cursor-pointer"
           onClick={() => {
@@ -81,7 +97,7 @@ export default function BottomSheet({ open }: BottomSheetProps) {
             if (isOpen) {
               setIsOpen(false);
             } else {
-              navigate("/");
+              setIsModalOpen(true);
             }
           }}
         />
