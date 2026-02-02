d9d1fded634bdcc837a24aabaeea6e87899e0c57
jangmk05 <jangmk05@gmail.com>
2026-02-02 21:23:46 +0900

    [FEAT] 영문 값 추가 & FormData 값 post

--- src/hooks/use-family-profile-form.ts
+++ src/hooks/use-family-profile-form.ts
@@ -1,9 +1,49 @@
 import { useEffect, useState } from "react";
 import { useProfileStore } from "../stores/use-profile-store";
+import { postProfile } from "../api/family-profile";
+import type { postProfileRequest } from "../types/family-profile";
+import { useFamilyStore } from "../stores/use-family-store";
 
 export const useFamilyProfileForm = () => {
   const { savedFormData, setSavedFormData } = useProfileStore();
   const [formData, setFormData] = useState(savedFormData);
+  const roleMap: Record<string, string> = {
+    엄마: "MOM",
+    아빠: "DAD",
+    아들: "SON",
+    딸: "DAUGHTER",
+  };
+  const preferenceMap: Record<string, string> = {
+    한식: "KOREAN",
+    중식: "CHINESE",
+    일식: "JAPANESE",
+    양식: "WESTERN",
+    디저트: "DESSERT",
+  };
+  const allergyMap: Record<string, string> = {
+    알류: "EGG",
+    우유: "MILK",
+    메밀: "BUCKWHEAT",
+    땅콩: "PEANUT",
+    대두: "SOYBEAN",
+    밀: "WHEAT",
+    잣: "PINE_NUT",
+    호두: "WALNUT",
+    게: "CRAB",
+    새우: "SHRIMP",
+    오징어: "SQUID",
+    고등어: "MACKEREL",
+    굴: "OYSTER",
+    홍합: "MUSSEL",
+    전복: "ABALONE",
+    복숭아: "PEACH",
+    토마토: "TOMATO",
+    닭고기: "CHICKEN",
+    돼지고기: "PORK",
+    쇠고기: "BEEF",
+    아황산류: "SULFITE",
+    젤라틴: "EXTRACTED_INGREDIENTS",
+  };
 
   useEffect(() => {
     setFormData(savedFormData);
@@ -92,9 +132,45 @@ export const useFamilyProfileForm = () => {
     );
   };
 
+  // 영문으로 변환
+  let allergyList: string[] | boolean;
+  if (Array.isArray(formData.allergies)) {
+    allergyList = formData.allergies.map((item) => allergyMap[item]);
+  } else {
+    allergyList = formData.allergies;
+  }
+
+  const preferencesList = formData.preferences.map(
+    (item) => preferenceMap[item],
+  );
+
   // 폼 제출 핸들러
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+
+    const request: postProfileRequest = {
+      nickname: formData.nickname,
+      role: roleMap[formData.role],
+      likedIngredients: formData.likedIngredients,
+      dislikedIngredients: formData.dislikedIngredients,
+      allergy: Array.isArray(allergyList) ? allergyList : [],
+      dietPreferences: preferencesList,
+    };
+
+    const currentFamilyRoomId = useFamilyStore.getState().familyRoomId;
+
+    if (currentFamilyRoomId === null) {
+      alert("가족방 정보가 존재하지 않습니다");
+      return;
+    }
+
+    try {
+      const res = await postProfile(currentFamilyRoomId, request);
+      console.log(res.result);
+    } catch (error) {
+      console.error("프로필 생성 실패:", error);
+    }
+
     if (!isValid()) {
       alert("필수 항목을 모두 입력해주세요.");
     }
