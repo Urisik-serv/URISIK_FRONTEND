867b819850f340b3579ee7ef168a51f778eef70d
shooukie <bigcape@naver.com>
2026-02-04 04:22:26 +0900

    [STYLE] 식단 탭 디자인 변경

--- src/pages/meal-plan/meal-plan-page.tsx
+++ src/pages/meal-plan/meal-plan-page.tsx
@@ -17,19 +17,19 @@ const MealPlanPage = () => {
       <div className="p-4 mt-1">
         <div className="w-full flex font-medium text-[14px] h-[42px]">
           <button
-            className={`flex-1 cursor-pointer border-b-1 ${tab === "오늘의 식단" ? "bg-gray-50 text-primary-700 border-primary-700" : "bg-white text-gray-300 border-gray-300"}`}
+            className={`flex-1 cursor-pointer border-b-1 ${tab === "오늘의 식단" ? "bg-primary-700 border-primary-700 rounded-t-lg text-white" : "bg-white text-gray-300 border-gray-300"}`}
             onClick={() => setTab("오늘의 식단")}
           >
             오늘의 식단
           </button>
           <button
-            className={`flex-1 cursor-pointer border-b-1 ${tab === "이번주 식단" ? "bg-gray-50 text-primary-700 border-primary-700" : "bg-white text-gray-300 border-gray-300"}`}
+            className={`flex-1 cursor-pointer border-b-1 ${tab === "이번주 식단" ? "bg-primary-700 border-primary-700 rounded-t-lg text-white" : "bg-white text-gray-300 border-gray-300"}`}
             onClick={() => setTab("이번주 식단")}
           >
             이번주 식단
           </button>
           <button
-            className={`flex-1 cursor-pointer border-b-1 ${tab === "다음주 식단" ? "bg-gray-50 text-primary-700 border-primary-700" : "bg-white text-gray-300 border-gray-300"}`}
+            className={`flex-1 cursor-pointer border-b-1 ${tab === "다음주 식단" ? "bg-primary-700 border-primary-700 rounded-t-lg text-white" : "bg-white text-gray-300 border-gray-300"}`}
             onClick={() => setTab("다음주 식단")}
           >
             다음주 식단
