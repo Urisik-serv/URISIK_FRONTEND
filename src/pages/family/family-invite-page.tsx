a126f3027de8a718e973d4981677080bf5ba5827
jangmk05 <jangmk05@gmail.com>
2026-02-05 17:19:11 +0900

    [FEAT] 카카오톡 가족방 초대 링크 연결

--- src/pages/family/family-invite-page.tsx
+++ src/pages/family/family-invite-page.tsx
@@ -4,9 +4,28 @@ import KakaoImage from "../../assets/images/kakao.png";
 import LinkImage from "../../assets/images/link.png";
 import Button from "../../components/common/Button";
 import { useNavigate } from "react-router-dom";
+import { useFamilyStore } from "../../stores/use-family-store";
+import { postInviteToken } from "../../api/invite";
 
 export default function FamilyInvitePage() {
   const navigate = useNavigate();
+  const { familyRoomId } = useFamilyStore();
+  const handleInvite = async () => {
+    if (!familyRoomId) {
+      alert(`가족방을 찾을 수 없습니다`);
+      return;
+    }
+    const res = await postInviteToken(familyRoomId);
+
+    window.Kakao.Share.sendDefault({
+      objectType: "text",
+      text: "가족방에 초대했어요!\n아래 링크를 눌러 참여해주세요",
+      link: {
+        mobileWebUrl: res.inviteUrl,
+        webUrl: res.inviteUrl,
+      },
+    });
+  };
   return (
     <>
       <PublicHeader title="가족초대" />
@@ -32,13 +51,19 @@ export default function FamilyInvitePage() {
             />
           </div>
           <div className="pt-[80px]">
-            <button className="cursor-pointer w-24 inline-flex flex-col justify-start items-center gap-2">
+            <button
+              onClick={handleInvite}
+              className="cursor-pointer w-24 inline-flex flex-col justify-start items-center gap-2"
+            >
               <img className="w-8 h-8" src={KakaoImage} />
               <div className="self-stretch text-center justify-start text-neutral-600 text-base font-normal font-['Wanted_Sans'] leading-6">
                 카카오톡
               </div>
             </button>
-            <button className="cursor-pointer w-24 inline-flex flex-col justify-start items-center gap-2">
+            <button
+              onClick={handleInvite}
+              className="cursor-pointer w-24 inline-flex flex-col justify-start items-center gap-2"
+            >
               <img className="w-8 h-8" src={LinkImage} />
               <div className="self-stretch text-center justify-start text-neutral-600 text-base font-normal font-['Wanted_Sans'] leading-6">
                 URL 복사
