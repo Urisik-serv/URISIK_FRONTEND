2517b76dc19e39f02a458af5bc0b8171da6e4158
jangmk05 <jangmk05@gmail.com>
2026-02-05 17:15:04 +0900

    [FEAT] 카카오 초기화 - 앱 시작시 한 번 만

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,16 @@ import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useEffect } from "react";
 
 function App() {
   const queryClient = new QueryClient();
+
+  useEffect(() => {
+    if (window.Kakao && !window.Kakao.isInitialized()) {
+      window.Kakao.init(import.meta.env.VITE_KAKAO_JS_KEY);
+    }
+  }, []);
 
   return (
     <>
