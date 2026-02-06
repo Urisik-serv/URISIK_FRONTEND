48fbb3ef0a8ae569d516a88d70e4cf1678ac8125
jangmk05 <jangmk05@gmail.com>
2026-02-06 13:55:51 +0900

    [FEAT] 가족 초대 화면 구현

--- src/pages/home/invite-entry.tsx
+++ src/pages/home/invite-entry.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AlertModal from "../../components/common/AlertModal";
 import HomePage from "./home-page";
 import { useNavigate, useParams } from "react-router-dom";
@@ -9,42 +9,53 @@ import { useMutation } from "@tanstack/react-query";
 
 export default function InviteEntry() {
   const [isOpen, setIsOpen] = useState(true);
+  const [inviterName, setInviterName] = useState<string | null>(null);
   const { token } = useParams();
   const { getItem: getAccessToken } = useLocalStorage("accessToken");
   const navigate = useNavigate();
   const { setFamilyRoomId } = useFamilyStore();
 
-  const getInviterName = async () => {
-    if (token == null) {
-      alert("유효하지 않은 토큰입니다.");
-      return;
-    }
+  useEffect(() => {
+    const fetchInviterName = async () => {
+      if (!token) return;
 
-    const res = await getInviteToken(token);
+      try {
+        const inviteRes = await getInviteToken(token);
+        if (inviteRes.expired) {
+          alert("유효하지 않은 토큰입니다.");
+          return;
+        }
 
-    if (res.expired) {
-      alert("유효하지 않은 토큰입니다.");
-      return;
-    }
+        if (!getAccessToken()) {
+          alert("로그인이 필요합니다");
+          navigate("/login");
+          return;
+        }
 
-    if (!getAccessToken) {
-      alert("로그인이 필요합니다");
-      navigate("/login");
-    }
+        setInviterName(inviteRes.inviterName);
+      } catch (error) {
+        console.error("초대 정보 조회 실패", error);
+      }
+    };
 
-    return res.inviterName;
-  };
+    fetchInviterName();
+  }, [token]);
 
   const { mutate: acceptMutation } = useMutation({
     mutationFn: () => {
-      if (!token) {
-        throw new Error("유효하지 않은 토큰입니다.");
-      }
+      if (!token) throw new Error("유효하지 않은 토큰입니다.");
       return postAcceptInvite(token);
     },
     onSuccess: (res) => {
       setFamilyRoomId(res.familyRoomId);
       navigate("/family-profile-create");
+    },
+    onError: (error: any) => {
+      if (error.response?.data?.code === "FAMILY_JOIN_409") {
+        navigate("/family-profile-create");
+      } else {
+        alert("참여 처리 중 오류가 발생했습니다.");
+      }
     },
   });
   return (
@@ -53,12 +64,12 @@ export default function InviteEntry() {
       {isOpen && (
         <AlertModal
           title={"초대장"}
-          boldContent={`${getInviterName}님의 가족구성원에 초대되었어요`}
+          boldContent={`${inviterName}님의 가족구성원에 초대되었어요`}
           mediumContent="우리 가족 식단을 함께 관리해요"
           buttonText="참여하기"
           outsideText="탭해서 닫기"
-          onClick={() => setIsOpen((prev) => !prev)}
-          handleModal={acceptMutation}
+          onClick={acceptMutation}
+          handleModal={() => setIsOpen((prev) => !prev)}
         />
       )}
     </>
