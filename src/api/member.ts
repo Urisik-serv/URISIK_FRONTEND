e7b5c3639bf312c6f9aa934c87f1b544a062f009
jangmk05 <jangmk05@gmail.com>
2026-02-07 12:40:07 +0900

    [MODIFY] 요청 바디 추가

--- src/api/member.ts
+++ src/api/member.ts
@@ -3,7 +3,9 @@ import { axiosInstance } from "./axios/axios";
 
 // 약관동의 api 연결
 export const patchAgree = async (request: Agree): Promise<Agree> => {
-  const { data } =
-    await axiosInstance.patch<AgreeResponse>(`/api/member/agree`);
+  const { data } = await axiosInstance.patch<AgreeResponse>(
+    `/api/member/agree`,
+    request,
+  );
   return data.result;
 };
