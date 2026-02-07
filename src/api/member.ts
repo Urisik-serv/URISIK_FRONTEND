46ac7b038c5cf22575b576bfe390ecde369f93bb
jangmk05 <jangmk05@gmail.com>
2026-02-07 12:39:22 +0900

    [FEAT] 약관 동의 api 호출

---
+++ src/api/member.ts
@@ -0,0 +1,9 @@
+import type { Agree, AgreeResponse } from "../types/member";
+import { axiosInstance } from "./axios/axios";
+
+// 약관동의 api 연결
+export const patchAgree = async (request: Agree): Promise<Agree> => {
+  const { data } =
+    await axiosInstance.patch<AgreeResponse>(`/api/member/agree`);
+  return data.result;
+};
