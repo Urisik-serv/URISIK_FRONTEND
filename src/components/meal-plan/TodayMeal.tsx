009c0313e10908488761c1223dec6c861b40a38c
shooukie <bigcape@naver.com>
2026-02-09 02:26:18 +0900

    [DESIGN] 오늘의 식단 디자인 수정사항 반영

--- src/components/meal-plan/TodayMeal.tsx
+++ src/components/meal-plan/TodayMeal.tsx
@@ -14,7 +14,7 @@ export default function TodayMeal({ data }: { data: TodayMeal }) {
         {isOpen && <ReviewModal recipeId={data.id} onClick={handleOpen} />}
         <div>
           {/* <p className="font-normal text-[16px] leading-[1.5]">우유대신,</p> */}
-          <p className="font-semibold text-[18px] tracking-[0.01em]">
+          <p className="font-semibold text-[18px] tracking-[0.01em] text-gray-600">
             {data.title.split(" ").map((title, idx) => (
               <>
                 {title} {idx % 2 === 1 && <br />}
@@ -25,7 +25,7 @@ export default function TodayMeal({ data }: { data: TodayMeal }) {
         <img
           src={data.imageUrl}
           alt={`${data.title} 이미지`}
-          className="w-41 h-29 object-cover"
+          className="w-41 h-29 object-cover rounded-lg"
         />
       </div>
       {!isDone ? (
@@ -52,18 +52,20 @@ export default function TodayMeal({ data }: { data: TodayMeal }) {
       <p className="font-semibold text-primary-700 text-[14px] pb-2">레시피</p>
       <div className="flex flex-col gap-4 pb-29">
         {data.recipeSteps.map((step) => (
-          <div className="h-full flex justify-between items-center text-[14px] font-semibold">
+          <div className="h-full flex justify-between items-center text-[14px] font-semibold gap-3">
             <div>
               <div className="flex flex-col gap-[1px] pb-[6px]">
-                <p>{step.stepOrder}단계</p>
+                <p className="text-gray-350">{step.stepOrder}단계</p>
                 {/* <p className=" text-[20px] ">재료손질</p> */}
               </div>
-              <p className="font-normal">{step.description.slice(3)}</p>
+              <p className="font-normal text-gray-800">
+                {step.description.slice(3)}
+              </p>
             </div>
             <img
               src={step.imageUrl}
               alt={`${step.stepOrder}단계 이미지`}
-              className="w-36 h-26 shrink-0 object-cover "
+              className="w-36 h-26 shrink-0 object-cover rounded-lg"
             />
           </div>
         ))}
