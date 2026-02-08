3c83ac803971b4f04131b22119dab37728d2b0ea
tablemin03 <vs201466@naver.com>
2026-02-08 20:15:09 +0900

    [FEAT] 드롭다운버튼 추가

--- src/components/home/curation/MealCuration.tsx
+++ src/components/home/curation/MealCuration.tsx
@@ -1,18 +1,19 @@
 import { useEffect, useState } from "react";
 import MealCard from "./MealCard";
-import type { FoodList } from "../../../types/recipes";
 import SampleImg from "../../../assets/sample/shrimp-mushroom.png";
-import axios from "axios";
+import type { RecommendPopularRecipes } from "../../../types/recipes";
+import { getRecommendScore } from "../../../api/recommendations";
+import SortDropdown from "../../common/SortDropDown";
 
 const MealCuration = () => {
-  // mock data fetching
-  const [data, setData] = useState<FoodList | null>(null);
+  const [sortType, setSortType] = useState("별점 순");
+  const [data, setData] = useState<RecommendPopularRecipes>();
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("/data/recipe-list.json");
+        const response = await getRecommendScore();
 
-        setData(response.data);
+        setData(response.result);
       } catch (error) {
         console.log("데이터 로딩 실패: ", error);
       }
@@ -23,7 +24,7 @@ const MealCuration = () => {
 
   return (
     <div className="pt-8">
-      <div>
+      <div className="pb-4">
         <h1 className="text-zinc-800 text-xl font-semibold tracking-tight">
           다음주에 이런 식단은 어떤가요?
         </h1>
@@ -31,15 +32,18 @@ const MealCuration = () => {
           식단에 추가하고 싶은 메뉴를 스크랩해보세요.
         </p>
       </div>
+      <div className="flex justify-end">
+        <SortDropdown onSortChange={setSortType} />
+      </div>
       {data?.recipes.map((recipe) => (
         <MealCard
           key={recipe.id}
-          id={String(recipe.id)}
-          shortDescription={recipe.shortDescription}
+          id={recipe.id}
+          shortDescription={recipe.description}
           title={recipe.title}
-          rating={recipe.rating}
+          rating={recipe.avgScore}
           category={recipe.category}
-          img={SampleImg}
+          img={recipe.imageUrl || SampleImg}
           external={null}
           type="RECIPE"
         />
