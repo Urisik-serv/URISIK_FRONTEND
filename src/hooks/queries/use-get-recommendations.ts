a1a7d280e12fc50bdebca11ce759ed9a7058142e
tablemin03 <vs201466@naver.com>
2026-02-08 20:46:46 +0900

    [MODIFY] 훅 분리

--- src/hooks/queries/use-get-recommendations.ts
+++ src/hooks/queries/use-get-recommendations.ts
@@ -1,41 +1,41 @@
 import { useQuery } from "@tanstack/react-query";
 import {
-  getRecommendSafe,
   getRecommendSafeScore,
   getRecommendScore,
   getRecommendWish,
 } from "../../api/recommendations";
 import { QUERY_KEY } from "../../constants/key";
 
-export const useGetRecommendSafe = () => {
-  return useQuery({
-    queryFn: () => getRecommendSafe(),
-    queryKey: [QUERY_KEY.safeRecipeTop],
-    staleTime: 1000 * 60 * 10,
-    gcTime: 1000 * 60 * 30,
-  });
+const getQueryConfig = (sortType: string, category?: string) => {
+  switch (sortType) {
+    case "안전한 순":
+      return {
+        queryKey: [QUERY_KEY.safeHighScore, category],
+        queryFn: () => getRecommendSafeScore(category),
+      };
+    case "찜 많은 순":
+      return {
+        queryKey: [QUERY_KEY.wishHighScore, category],
+        queryFn: () => getRecommendWish(category),
+      };
+    case "별점 순":
+    default:
+      return {
+        queryKey: [QUERY_KEY.highScore, category],
+        queryFn: () => getRecommendScore(category),
+      };
+  }
 };
-export const useGetRecommendScore = (category?: string) => {
+
+export const useGetRecommendList = (sortType: string, category?: string) => {
+  const { queryKey, queryFn } = getQueryConfig(sortType, category);
+
   return useQuery({
-    queryFn: () => getRecommendScore(category),
-    queryKey: [QUERY_KEY.highScore],
+    queryKey,
+    queryFn,
     staleTime: 1000 * 60 * 10,
     gcTime: 1000 * 60 * 30,
-  });
-};
-export const useGetRecommendSafeScore = (category?: string) => {
-  return useQuery({
-    queryFn: () => getRecommendSafeScore(category),
-    queryKey: [QUERY_KEY.safeHighScore],
-    staleTime: 1000 * 60 * 10,
-    gcTime: 1000 * 60 * 30,
-  });
-};
-export const useGetRecommendWish = (category?: string) => {
-  return useQuery({
-    queryFn: () => getRecommendWish(category),
-    queryKey: [QUERY_KEY.wishHighScore],
-    staleTime: 1000 * 60 * 10,
-    gcTime: 1000 * 60 * 30,
+
+    select: (data) => data.result,
   });
 };
---
+++ src/hooks/queries/use-get-safe-recipes.ts
@@ -0,0 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+import { getRecommendSafe } from "../../api/recommendations";
+import { QUERY_KEY } from "../../constants/key";
+
+const useGetRecommendSafe = () => {
+  return useQuery({
+    queryFn: () => getRecommendSafe(),
+    queryKey: [QUERY_KEY.safeRecipeTop],
+    staleTime: 1000 * 60 * 10,
+    gcTime: 1000 * 60 * 30,
+  });
+};
+
+export default useGetRecommendSafe;
