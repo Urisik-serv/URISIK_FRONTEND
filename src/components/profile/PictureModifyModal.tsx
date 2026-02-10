dd1c4d2cd54b772072e31385bf008f7ff8f9088e
jangmk05 <jangmk05@gmail.com>
2026-02-11 01:02:40 +0900

    [STYLE] 취소 버튼 삭제

--- src/components/profile/PictureModifyModal.tsx
+++ src/components/profile/PictureModifyModal.tsx
@@ -54,7 +54,10 @@ export default function PictureModifyModal({ onClick }: PictureModalProps) {
 
   return (
     <>
-      <div className="fixed inset-0 z-50 bg-black/30 flex flex-col items-center justify-end">
+      <div
+        onClick={onClick}
+        className="fixed inset-0 z-50 bg-black/30 flex flex-col items-center justify-end"
+      >
         <div className="pb-[26px] w-[353px] flex flex-col items-center gap-[8px]">
           <div className="self-stretch h-40 py-4 bg-[#FF885A] rounded-[10px] outline-none inline-flex flex-col justify-start items-center gap-4">
             <button
@@ -103,14 +106,6 @@ export default function PictureModifyModal({ onClick }: PictureModalProps) {
               {isCheck === "default" && <Check />}{" "}
             </button>
           </div>
-          <button
-            onClick={onClick}
-            className="cursor-pointer self-stretch h-14 px-40 py-4 bg-zinc-100 rounded-[10px] inline-flex justify-center items-center gap-2.5"
-          >
-            <div className="text-center text-zinc-800 text-lg font-medium  leading-5">
-              취소
-            </div>
-          </button>
         </div>
       </div>
     </>
