e5369827728b9c843ff54ecb9831e98b0fcfb509
tablemin03 <vs201466@naver.com>
2026-02-10 22:40:04 +0900

    [REFACTOR] 상세 메뉴 페이지에서 설명 분리 및 스타일 수정

--- src/pages/home/menu-information-page.tsx
+++ src/pages/home/menu-information-page.tsx
@@ -10,6 +10,7 @@ import type { DetailRecipe } from "../../types/recipes";
 import { useFamilyStore } from "../../stores/use-family-store";
 import usePostWishList from "../../hooks/mutations/use-post-wishlist";
 import ImageIndicator from "../../components/home/detailPage/ImageIndicator";
+import DetailContent from "../../components/home/detailPage/detailContent";
 
 const MenuInformationPage = () => {
   const { menuId } = useParams();
@@ -66,41 +67,7 @@ const MenuInformationPage = () => {
 
       <div className="px-4 pt-8 rounded-t-3xl -mt-10 relative z-10 bg-white">
         <div>
-          <h1 className="text-2xl font-semibold pb-3 leading-9 text-gray-800">
-            {recipe?.title}
-          </h1>
-          <div className="flex justify-start gap-2 pb-10">
-            <p className="text-gray-400 text-base font-medium leading-6">
-              {recipe?.category}
-            </p>
-            <Rate px={16} rate={4.5} />
-          </div>
-          <div className="pb-6">
-            <div className="flex gap-1 px-3 py-2.5 bg-gray-50 rounded-lg flex-col">
-              <div className="flex flex-row gap-2">
-                <p className="text-gray-800 text-[14px] font-medium leading-6">
-                  알레르기 위험도
-                </p>
-                <p
-                  className={`text-white text-xs font-semibold leading-4 px-[7px] py-px flex items-center rounded-xl ${recipe?.allergyWarning.hasRisk ? "bg-primary-700" : "bg-teal-400"}`}
-                >
-                  {recipe?.allergyWarning.hasRisk ? "위험" : "안전"}
-                </p>
-              </div>
-              {recipe?.allergyWarning.hasRisk ?? (
-                <p className="text-[13px] text-gray-500 font-medium leading-5">
-                  우리가족 버전으로 변경하면, 다른 식재료로 대체됩니다. 다만,
-                  원재료와는 맛이 다를 수 있습니다.
-                </p>
-              )}
-            </div>
-          </div>
-          <div className="pb-5">
-            <MenuInfo title="재료" sentences={recipe?.ingredients} />
-          </div>
-          <div className="pb-20">
-            <MenuInfo title="레시피" recipes={recipe?.steps} />
-          </div>
+          <DetailContent recipe={recipe} />
           <div className="fixed left-1/2 -translate-x-1/2 w-full max-w-[375px] px-4 pb-3 z-50 bottom-0">
             <WishlistButton
               onClick={handleClick}
