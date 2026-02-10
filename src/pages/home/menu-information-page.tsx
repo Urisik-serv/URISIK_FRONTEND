76f3bba0052a80adae65bf7b1b57a4f52f23daf5
tablemin03 <vs201466@naver.com>
2026-02-10 22:30:22 +0900

    [REFACTOR] 사진 부분 컴포넌트 분리

--- src/pages/home/menu-information-page.tsx
+++ src/pages/home/menu-information-page.tsx
@@ -1,29 +1,32 @@
 import { useEffect, useState } from "react";
 import PublicHeader from "../../components/header/PublicHeader";
-import { useParams } from "react-router-dom";
-import WhiteHeart from "../../assets/icons/heart-white.svg";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import Rate from "../../components/common/Rate";
 import MenuInfo from "../../components/common/MenuInfo";
 import UpButton from "../../components/common/UpButton";
-import PageIndicator from "../../components/common/PageIndicator";
 import WishlistButton from "../../components/common/WishlistButton";
-import { getDetailRecipe } from "../../api/recipes";
+import { getDetailRecipe, postTransRecipe } from "../../api/recipes";
 import type { DetailRecipe } from "../../types/recipes";
 import { useFamilyStore } from "../../stores/use-family-store";
 import usePostWishList from "../../hooks/mutations/use-post-wishlist";
+import ImageIndicator from "../../components/home/detailPage/ImageIndicator";
 
 const MenuInformationPage = () => {
   const { menuId } = useParams();
   const [recipe, setRecipe] = useState<DetailRecipe>();
+
+  const navigate = useNavigate();
+  const type = new URLSearchParams(useLocation().search).get("type");
+  const recipeId = Number(menuId);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const recipeId = Number(menuId);
         const data = await getDetailRecipe(recipeId);
         console.log("get 요청 성공: ", data);
         setRecipe(data.result);
       } catch (error) {
-        console.log("데이터 로딩 실패: ", error);
+        console.log("일반 레시피 로딩 실패: ", error);
       }
     };
     fetchData();
@@ -32,54 +35,65 @@ const MenuInformationPage = () => {
   const roomId = useFamilyStore.getState().familyRoomId;
   const { mutate: addWishList } = usePostWishList(roomId);
 
+  const isWishList = recipe?.allergyWarning.hasRisk ? false : true;
+
   const handleClick = async () => {
-    const currentRecipeId = recipe?.recipeId;
+    if (isWishList) {
+      const currentRecipeId = recipe?.recipeId;
 
-    const directPayload = {
-      recipeId: currentRecipeId ? [currentRecipeId] : [],
-      transformedRecipeId: [],
-    };
+      const directPayload = {
+        recipeId: currentRecipeId ? [currentRecipeId] : [],
+        transformedRecipeId: [],
+      };
 
-    addWishList(directPayload);
+      addWishList(directPayload);
+    } else {
+      const transData = await postTransRecipe(recipeId);
+      navigate(
+        `/menu-information/${transData.result.transformedRecipeId}?type=TRANSFORMED`,
+      );
+    }
   };
 
   return (
     <div>
       <PublicHeader title={"메뉴 정보"} />
-      <div className="relative h-72 w-full">
-        <img
-          src={recipe?.images.small}
-          alt={`${recipe?.title} 사진`}
-          className="h-full w-full object-cover shrink-0"
-        />
-        <div className="absolute bottom-16 left-0 w-full flex justify-end pr-2 z-0">
-          <div className="absolute left-1/2 -translate-x-1/2">
-            <PageIndicator page={1} total={1} />
-          </div>
-          <div className="px-2.5 py-2 bg-primary-700 flex justify-start items-center rounded-2xl gap-1">
-            <img src={WhiteHeart} alt="좋아요" />
-            <p className="text-white font-semibold text-[10px]">
-              {recipe?.wishCount}가구가 선택했어요
-            </p>
-          </div>
-        </div>
-      </div>
+      <ImageIndicator
+        imgUrl={recipe?.images.small}
+        name={recipe?.title}
+        wishCount={recipe?.wishCount}
+      />
+
       <div className="px-4 pt-8 rounded-t-3xl -mt-10 relative z-10 bg-white">
         <div>
           <h1 className="text-2xl font-semibold pb-3 leading-9 text-gray-800">
             {recipe?.title}
           </h1>
-          <div className="flex justify-start gap-2 pb-12">
+          <div className="flex justify-start gap-2 pb-10">
             <p className="text-gray-400 text-base font-medium leading-6">
               {recipe?.category}
             </p>
             <Rate px={16} rate={4.5} />
           </div>
-          <div className="pb-12">
-            {/*<MenuInfo
-              title="어떻게 바뀌었나요?"
-              sentences={recipe?.descriptions}
-            />*/}
+          <div className="pb-6">
+            <div className="flex gap-1 px-3 py-2.5 bg-gray-50 rounded-lg flex-col">
+              <div className="flex flex-row gap-2">
+                <p className="text-gray-800 text-[14px] font-medium leading-6">
+                  알레르기 위험도
+                </p>
+                <p
+                  className={`text-white text-xs font-semibold leading-4 px-[7px] py-px flex items-center rounded-xl ${recipe?.allergyWarning.hasRisk ? "bg-primary-700" : "bg-teal-400"}`}
+                >
+                  {recipe?.allergyWarning.hasRisk ? "위험" : "안전"}
+                </p>
+              </div>
+              {recipe?.allergyWarning.hasRisk ?? (
+                <p className="text-[13px] text-gray-500 font-medium leading-5">
+                  우리가족 버전으로 변경하면, 다른 식재료로 대체됩니다. 다만,
+                  원재료와는 맛이 다를 수 있습니다.
+                </p>
+              )}
+            </div>
           </div>
           <div className="pb-5">
             <MenuInfo title="재료" sentences={recipe?.ingredients} />
@@ -89,8 +103,8 @@ const MenuInformationPage = () => {
           </div>
           <div className="fixed left-1/2 -translate-x-1/2 w-full max-w-[375px] px-4 pb-3 z-50 bottom-0">
             <WishlistButton
-              isWishList={!recipe?.allergyWarning.hasRisk}
               onClick={handleClick}
+              isSafe={!recipe?.allergyWarning.hasRisk}
             />
           </div>
           <UpButton />
