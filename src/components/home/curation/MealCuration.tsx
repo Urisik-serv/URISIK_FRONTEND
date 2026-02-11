6e419c4e1f3e5820396a7cb8e8748c4e3a3216de
tablemin03 <vs201466@naver.com>
2026-02-12 02:49:25 +0900

    [DESIGN] 정렬 모달 디자인 수정

--- src/components/home/curation/MealCuration.tsx
+++ src/components/home/curation/MealCuration.tsx
@@ -2,7 +2,9 @@ import { useState } from "react";
 import MealCard from "./MealCard";
 import SampleImg from "../../../assets/sample/shrimp-mushroom.png";
 import { useGetRecommendList } from "../../../hooks/queries/use-get-recommendations";
-import SortDropdown from "../../common/SortDropdown";
+import SelectionModal from "../../common/SelectionModal";
+import MenuDown from "../../../assets/icons/menu-down.svg";
+import MenuUp from "../../../assets/icons/menu-up.svg";
 
 interface MealCurationProps {
   category: string | undefined;
@@ -11,6 +13,9 @@ const MealCuration = ({ category }: MealCurationProps) => {
   const [sortType, setSortType] = useState("별점 순");
 
   const { data } = useGetRecommendList(sortType, category);
+
+  // SelectionModal state
+  const [openModal, setOpenModal] = useState(false);
 
   return (
     <div className="pt-8">
@@ -23,7 +28,19 @@ const MealCuration = ({ category }: MealCurationProps) => {
         </p>
       </div>
       <div className="flex justify-end">
-        <SortDropdown onSortChange={setSortType} />
+        <div className="relative">
+          <button
+            onClick={() => setOpenModal(true)}
+            className={`flex items-center justify-between w-full pl-3 pr-2 py-2 gap-1 bg-gray-100 text-base font-medium text-gray-800 hover:text-gray-900 cursor-pointer ${openModal ? "rounded-t-lg" : "rounded-lg"}`}
+          >
+            <span>{sortType}</span>
+            <img
+              className="w-4 h-4"
+              src={openModal ? MenuUp : MenuDown}
+              alt={openModal ? "닫기" : "열기"}
+            />
+          </button>
+        </div>
       </div>
       {data?.recipes.map((recipe) => (
         <MealCard
@@ -40,6 +57,22 @@ const MealCuration = ({ category }: MealCurationProps) => {
           typeBool={recipe.transformed}
         />
       ))}
+      {openModal && (
+        <SelectionModal
+          handleModal={() => setOpenModal(false)}
+          isCheck={sortType}
+          bgColor="bg-white"
+          borderColor="border-gray-400"
+          textColor="text-gray-700"
+          firstSelectionOnClick={() => setSortType("별점 순")}
+          firstSelectionText="별점 순"
+          secondSelectionOnClick={() => setSortType("안전한 순")}
+          secondSelectionText="안전한 순"
+          thirdSelectionOnClick={() => setSortType("찜 많은 순")}
+          thirdSelectionText="찜 많은 순"
+          checkIconColor="#9CA3AF"
+        />
+      )}
     </div>
   );
 };
