f6ab26abf2275d22615932685c389964c6e3edd6
jangmk05 <jangmk05@gmail.com>
2026-02-11 21:55:26 +0900

    [BUILD] Toast 라이브러리 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,18 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import SSEProvider from "./providers/SSE-provider";
 import KakaoProvider from "./providers/kakao-provider";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   const queryClient = new QueryClient();
 
   return (
     <>
+      <Toaster />
       <QueryClientProvider client={queryClient}>
         <KakaoProvider>
-          <SSEProvider>
-            <RouterProvider router={router} />
-          </SSEProvider>
+          <RouterProvider router={router} />
         </KakaoProvider>
         {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
       </QueryClientProvider>
