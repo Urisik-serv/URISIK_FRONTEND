ae6af672162f66ea5bd0c805c6eaece33f10108f
jangmk05 <jangmk05@gmail.com>
2026-02-11 12:35:51 +0900

    [FEAT] 알림 관련 API 연결 완료

--- src/api/notifications.ts
+++ src/api/notifications.ts
@@ -1,12 +1,38 @@
-import type { ResponseNotice } from "../types/notice-list";
+import type {
+  ResponseIsRead,
+  ResponseNotice,
+  ResponseSSE,
+} from "../types/notice-list";
 import { axiosInstance } from "./axios/axios";
 
+// 알림 목록 조회
 export const getNotificationList = async (
   size: number,
 ): Promise<ResponseNotice> => {
-  const { data } = await axiosInstance.get("/api/notifications", {
-    params: { size: size },
-  });
+  const { data } = await axiosInstance.get<ResponseNotice>(
+    "/api/notifications",
+    {
+      params: { size: size },
+    },
+  );
 
+  return data;
+};
+
+// 알림용 SSE 연결
+export const getSSE = async (): Promise<ResponseSSE> => {
+  const { data } = await axiosInstance.get<ResponseSSE>(
+    "/api/notifications/subscribe",
+  );
+  return data;
+};
+
+// 알림 읽음 처리
+export const patchIsRead = async (
+  notificationId: number,
+): Promise<ResponseIsRead> => {
+  const { data } = await axiosInstance.patch<ResponseIsRead>(
+    `/api/notifications/${notificationId}/read`,
+  );
   return data;
 };
