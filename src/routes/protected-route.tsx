da717150ed9c3f22379b3348544f6458e9b15bd1
jangmk05 <jangmk05@gmail.com>
2026-02-11 14:27:30 +0900

    [REFACTOR] useLocalStorage 훅으로 통일

--- src/routes/protected-route.tsx
+++ src/routes/protected-route.tsx
@@ -1,9 +1,11 @@
 import { Navigate, Outlet } from "react-router-dom";
+import { useLocalStorage } from "../hooks/use-local-storage";
 
 export default function ProtectedRoute() {
   // 실제 앱에서는 전역 상태(Zustand 등)나 쿠키/스토리지 체크
-  const isAuthenticated = !!localStorage.getItem("accessToken");
-  console.log(isAuthenticated);
+  const { getItem: getAccessToken } = useLocalStorage("accessToken");
+  const isAuthenticated = !!getAccessToken();
+
   if (!isAuthenticated) {
     // 지금은 인증이 없을 때 접근 가능하게 해놓음
     return <Navigate to="/login" replace />;
