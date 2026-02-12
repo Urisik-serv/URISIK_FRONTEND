0c79849b8172a798b4b688c6a0997b12007abaf6
tablemin03 <vs201466@naver.com>
2026-02-12 19:20:22 +0900

    [FEAT] 내 위시리스트 스켈레톤 추가

--- src/components/wish-list/MyWishList.tsx
+++ src/components/wish-list/MyWishList.tsx
@@ -11,6 +11,7 @@ import { useInView } from "react-intersection-observer";
 import useDeleteProfileWishLists from "../../hooks/mutations/use-delete-profile-wishlists";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
+import MenuListSkeleton from "../skeltons/MenuListSkeleton";
 
 const MyWishList = () => {
   const [editMode, setEditMode] = useState(false);
@@ -22,6 +23,7 @@ const MyWishList = () => {
     hasNextPage: profileNext,
     fetchNextPage: fetchProfile,
     isError: errorProfile,
+    isLoading: loadingProfile,
   } = useGetInfiniteProfileWishList(roomId, -1, 3);
 
   const {
@@ -30,9 +32,12 @@ const MyWishList = () => {
     hasNextPage: transNext,
     fetchNextPage: fetchTrans,
     isError: errorTrans,
+    isLoading: loadingTrans,
   } = useGetInfiniteProfileTransWishList(roomId, -1, 5);
 
   const navigate = useNavigate();
+
+  const loading = loadingProfile || loadingTrans;
 
   const { ref, inView } = useInView({
     threshold: 0,
@@ -97,86 +102,98 @@ const MyWishList = () => {
           isEditMode={editMode}
         />
       </div>
-      <div className="pt-2">
-        {transWish?.pages.map((item) => {
-          const uniqueKey = getUniqueKey(null, item.transformedRecipeId);
+      {loading ? (
+        <>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <MenuListSkeleton key={index} />
+          ))}
+        </>
+      ) : (
+        <>
+          <div className="pt-2">
+            {transWish?.pages.map((item) => {
+              const uniqueKey = getUniqueKey(null, item.transformedRecipeId);
 
-          return (
-            <div
-              key={item.transformedRecipeId}
-              className="flex items-center border-b border-gray-200"
-            >
-              {editMode && (
-                <img
-                  className="cursor-pointer w-6 h-6 shrink-0"
-                  src={`${isSelected(uniqueKey) ? CheckedBox : EmptyBox}`}
-                  alt={`${isSelected(uniqueKey) ? "선택됨" : "선택되지 않음"}`}
-                  onClick={() => {
-                    toggleSelection(uniqueKey);
-                  }}
-                />
-              )}
-              <div className="flex-1 min-w-0">
-                <MenuList
+              return (
+                <div
                   key={item.transformedRecipeId}
-                  type="default"
-                  menu={item.transformedRecipeName}
-                  isSafe={item.foodSafety}
-                  category={item.category}
-                  ingredients={item.recipeIngredients}
-                  img={item.foodImage}
-                  clickable={true}
-                  onClick={() =>
-                    navigate(
-                      `/menu-information/${item.transformedRecipeId}?type=TRANSFORMED`,
-                    )
-                  }
-                  hasBorder={false}
-                />
-              </div>
-            </div>
-          );
-        })}
-      </div>
-      <div>
-        {profileWish?.pages.map((item) => {
-          const uniqueKey = getUniqueKey(item.recipeId, null);
+                  className="flex items-center border-b border-gray-200"
+                >
+                  {editMode && (
+                    <img
+                      className="cursor-pointer w-6 h-6 shrink-0"
+                      src={`${isSelected(uniqueKey) ? CheckedBox : EmptyBox}`}
+                      alt={`${isSelected(uniqueKey) ? "선택됨" : "선택되지 않음"}`}
+                      onClick={() => {
+                        toggleSelection(uniqueKey);
+                      }}
+                    />
+                  )}
+                  <div className="flex-1 min-w-0">
+                    <MenuList
+                      key={item.transformedRecipeId}
+                      type="default"
+                      menu={item.transformedRecipeName}
+                      isSafe={item.foodSafety}
+                      category={item.category}
+                      ingredients={item.recipeIngredients}
+                      img={item.foodImage}
+                      clickable={true}
+                      onClick={() =>
+                        navigate(
+                          `/menu-information/${item.transformedRecipeId}?type=TRANSFORMED`,
+                        )
+                      }
+                      hasBorder={false}
+                    />
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <div>
+            {profileWish?.pages.map((item) => {
+              const uniqueKey = getUniqueKey(item.recipeId, null);
 
-          return (
-            <div
-              key={item.recipeId}
-              className="flex items-center border-b border-gray-200"
-            >
-              {editMode && (
-                <img
-                  className="cursor-pointer w-6 h-6 shrink-0"
-                  src={`${isSelected(uniqueKey) ? CheckedBox : EmptyBox}`}
-                  alt={`${isSelected(uniqueKey) ? "선택됨" : "선택되지 않음"}`}
-                  onClick={() => {
-                    toggleSelection(uniqueKey);
-                  }}
-                />
-              )}
-              <div className="flex-1 min-w-0">
-                <MenuList
+              return (
+                <div
                   key={item.recipeId}
-                  type="default"
-                  menu={item.recipeName}
-                  category={item.category}
-                  isSafe={item.foodSafety}
-                  ingredients={item.recipeIngredients}
-                  img={item.foodImage}
-                  clickable={true}
-                  onClick={() =>
-                    navigate(`/menu-information/${item.recipeId}?type=RECIPE`)
-                  }
-                  hasBorder={false}
-                />
-              </div>
-            </div>
-          );
-        })}
-      </div>
+                  className="flex items-center border-b border-gray-200"
+                >
+                  {editMode && (
+                    <img
+                      className="cursor-pointer w-6 h-6 shrink-0"
+                      src={`${isSelected(uniqueKey) ? CheckedBox : EmptyBox}`}
+                      alt={`${isSelected(uniqueKey) ? "선택됨" : "선택되지 않음"}`}
+                      onClick={() => {
+                        toggleSelection(uniqueKey);
+                      }}
+                    />
+                  )}
+                  <div className="flex-1 min-w-0">
+                    <MenuList
+                      key={item.recipeId}
+                      type="default"
+                      menu={item.recipeName}
+                      category={item.category}
+                      isSafe={item.foodSafety}
+                      ingredients={item.recipeIngredients}
+                      img={item.foodImage}
+                      clickable={true}
+                      onClick={() =>
+                        navigate(
+                          `/menu-information/${item.recipeId}?type=RECIPE`,
+                        )
+                      }
+                      hasBorder={false}
+                    />
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </>
+      )}
       <div ref={ref} className="h-2"></div>
     </div>
   );
