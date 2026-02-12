4a0beac89059e98bada5e83e99da9e5c4a780972
tablemin03 <vs201466@naver.com>
2026-02-12 14:47:43 +0900

    [FEAT] 우리 가족 버전으로 바꿀 때, 로딩스피너 추가

--- src/pages/home/menu-information-page.tsx
+++ src/pages/home/menu-information-page.tsx
@@ -16,10 +16,12 @@ import {
 } from "../../hooks/queries/use-my-wishlist-ids";
 import { useMyProfileStore } from "../../stores/use-my-profile-store";
 import useDeleteProfileWishLists from "../../hooks/mutations/use-delete-profile-wishlists";
+import { LoadingSpinner } from "../../components/common/LoadingSpinner";
 
 const MenuInformationPage = () => {
   const { menuId } = useParams();
   const [recipe, setRecipe] = useState<DetailRecipe>();
+  const [isLoading, setIsLoading] = useState(false);
 
   const navigate = useNavigate();
   const recipeId = Number(menuId);
@@ -64,46 +66,60 @@ const MenuInformationPage = () => {
     } else if (isSafe) {
       addWishList(directPayload);
     } else {
-      const transData = await postTransRecipe(recipeId);
-      navigate(
-        `/menu-information/${transData.result.transformedRecipeId}?type=TRANSFORMED`,
-      );
+      setIsLoading(true); //로딩 시작
+      try {
+        const transData = await postTransRecipe(recipeId);
+        navigate(
+          `/menu-information/${transData.result.transformedRecipeId}?type=TRANSFORMED`,
+        );
+      } catch (error) {
+        console.log("우리가족 버전 생성 실패", error);
+        setIsLoading(false);
+      }
     }
   };
 
   return (
     <div>
       <PublicHeader title={"메뉴 정보"} />
-      <ImageIndicator
-        imgUrl={recipe?.images.small || recipe?.images.large}
-        name={recipe?.title}
-        wishCount={recipe?.wishCount}
-      />
-
-      <div className="px-4 pt-8 rounded-t-3xl -mt-10 relative z-10 bg-white">
-        <div className="pb-20">
-          <div>
-            <h1 className="text-2xl font-semibold pb-3 leading-9 text-gray-800">
-              {recipe?.title}
-            </h1>
-            <div className="flex justify-start gap-2 pb-10">
-              <p className="text-gray-400 text-base font-medium leading-6">
-                {recipe?.category}
-              </p>
-              <Rate px={16} rate={recipe?.avgScore} />
-            </div>
-            <DetailContent recipe={recipe} />
-          </div>
-          <div className="fixed left-1/2 -translate-x-1/2 w-full max-w-[375px] px-4 pb-3 z-50 bottom-0">
-            <WishlistButton
-              onClick={handleClick}
-              isSafe={isSafe}
-              isWishList={isWishList}
-            />
-          </div>
-          <UpButton />
+      {isLoading ? (
+        <div className="flex flex-1 h-[calc(100vh-60px)] justify-center items-center">
+          <LoadingSpinner text="AI가 사용자에 맞춰서 레시피를 생성하고 있어요" />
         </div>
-      </div>
+      ) : (
+        <>
+          <ImageIndicator
+            imgUrl={recipe?.images.small || recipe?.images.large}
+            name={recipe?.title}
+            wishCount={recipe?.wishCount}
+          />
+
+          <div className="px-4 pt-8 rounded-t-3xl -mt-10 relative z-10 bg-white">
+            <div className="pb-20">
+              <div>
+                <h1 className="text-2xl font-semibold pb-3 leading-9 text-gray-800">
+                  {recipe?.title}
+                </h1>
+                <div className="flex justify-start gap-2 pb-10">
+                  <p className="text-gray-400 text-base font-medium leading-6">
+                    {recipe?.category}
+                  </p>
+                  <Rate px={16} rate={recipe?.avgScore} />
+                </div>
+                <DetailContent recipe={recipe} />
+              </div>
+              <div className="fixed left-1/2 -translate-x-1/2 w-full max-w-[375px] px-4 pb-3 z-50 bottom-0">
+                <WishlistButton
+                  onClick={handleClick}
+                  isSafe={isSafe}
+                  isWishList={isWishList}
+                />
+              </div>
+              <UpButton />
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
