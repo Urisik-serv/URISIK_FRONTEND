332f594c850b5dacc41bc816250eed5a8c4fa5b0
tablemin03 <vs201466@naver.com>
2026-03-05 17:30:10 +0900

    [FEAT] 리프레시 토큰 인터셉터 추가& 로그아웃 로직
    [FEAT] 리프레시 토큰 인터셉터 추가& 로그아웃 로직

--- src/api/axios/axios.ts
+++ src/api/axios/axios.ts
@@ -1,9 +1,15 @@
 import axios from "axios";
 import { useLocalStorage } from "../../hooks/use-local-storage";
 import { LOCAL_STORAGE_KEY } from "../../constants/key";
+import { postLogout, postReissue } from "../auth";
+import toast from "react-hot-toast";
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || "",
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 // 요청 인터셉터: 모든 요청 전에 accessToken을 Authorization 헤더에 추가
@@ -23,4 +29,41 @@ axiosInstance.interceptors.request.use(
   },
   // 요청 인터셉터가 실패하면, 에러 뿜음
   (error) => Promise.reject(error),
+);
+
+// 응답 인터셉터
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    // 401에러 & 재시도 X
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const reissueRes = await postReissue();
+
+        originalRequest.headers.Authorization = `Bearer ${reissueRes.result.accessToken}`;
+        if (reissueRes.result.needAgreement) {
+          // 약관동의 안 돼있을 경우 약관동의로 리다이렉트
+          window.location.href = "/agreement";
+        }
+        return axiosInstance(originalRequest);
+      } catch (refreshError) {
+        // 리프레시 토큰 만료
+        toast.error("세션이 만료되었습니다. 다시 로그인해주세요.");
+
+        try {
+          await postLogout();
+        } catch (logoutError) {
+          toast.error("로그아웃 API 호출 실패");
+        } finally {
+          localStorage.removeItem(LOCAL_STORAGE_KEY.accessToken);
+          window.location.href = "/login";
+        }
+      }
+    }
+    return Promise.reject(error);
+  },
 );
