fe69125b77fb80c268df66f199f356fd60ebf0a6
tablemin03 <vs201466@naver.com>
2026-03-06 14:14:41 +0900

    [REFACTOR] 일관성을 위한 axiosInstance 사용

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,16 +1,13 @@
-import axios from "axios";
 import type { LogoutResponse, ReissueResponse } from "../types/auth";
+import { axiosInstance } from "./axios/axios";
 
 export const postReissue = async (): Promise<ReissueResponse> => {
-  const { data } = await axios.post<ReissueResponse>(
-    `${import.meta.env.VITE_API_BASE_URL}/api/auth/reissue`,
-  );
+  const { data } =
+    await axiosInstance.post<ReissueResponse>("/api/auth/reissue");
   return data;
 };
 
 export const postLogout = async (): Promise<LogoutResponse> => {
-  const { data } = await axios.post<LogoutResponse>(
-    `${import.meta.env.VITE_API_BASE_URL}/api/auth/logout`,
-  );
+  const { data } = await axiosInstance.post<LogoutResponse>("/api/auth/logout");
   return data;
 };
