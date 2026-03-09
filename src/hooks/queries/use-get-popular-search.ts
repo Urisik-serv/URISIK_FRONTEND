697a7c07e7b3f95ec66fa11e56202f9ff49ce324
tablemin03 <vs201466@naver.com>
2026-03-09 13:07:48 +0900

    [FEAT] PopularSearch 쿼리 추가

---
+++ src/hooks/queries/use-get-popular-search.ts
@@ -0,0 +1,11 @@
+import { useQuery } from "@tanstack/react-query";
+import { getPopularSearch } from "../../api/search";
+
+const useGetPopularSearch = () => {
+  return useQuery({
+    queryFn: () => getPopularSearch(),
+    queryKey: ["popularSearch"],
+    staleTime: 1000 * 60 * 60,
+    gcTime: 1000 * 60 * 60,
+  });
+};
