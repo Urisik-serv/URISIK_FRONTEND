8c19fb5a85166ee337491af26b33e1de409f9317
tablemin03 <vs201466@naver.com>
2026-03-20 14:45:59 +0900

    [MODIFY] 잘못된 쿼리키 수정

--- src/hooks/queries/use-get-recommend-search.ts
+++ src/hooks/queries/use-get-recommend-search.ts
@@ -5,7 +5,7 @@ import { queryFactory } from "./query-factory";
 const useGetRecommendSearch = (familyRoomId: number | null) => {
   return useQuery({
     queryFn: () => getRecommendSearch(familyRoomId),
-    queryKey: queryFactory.search.recommendSearch(),
+    queryKey: queryFactory.recipe.recommendSearch(),
 
     staleTime: 1000 * 60 * 30,
     gcTime: 1000 * 60 * 30,
