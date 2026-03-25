6cc7e8363a7f79ae8209fbd12294325de8a47a00
shooukie <bigcape@naver.com>
2026-03-25 22:58:47 +0900

    [MERGE] 머지 충돌 해결(변경사항 적용)

--- src/pages/meal-plan/meal-plan-edit-page.tsx
+++ src/pages/meal-plan/meal-plan-edit-page.tsx
@@ -424,16 +424,16 @@ function BottomSheet({
 
       <div className="fixed bottom-0 left-1/2 -translate-x-1/2 flex justify-center pb-10">
         <Button
-          text={isOpen ? "바꾸기" : "수정완료"}
+          size="Btn_L"
+          variant="primary"
           type="button"
-          onClick={() => {
-            if (isOpen) {
-              handleChange();
-            } else {
-              setIsModalOpen(true);
-            }
-          }}
-        />
+          className="w-[343px]"
+          onClick={handleChange}
+        >
+          <span className="text-xl font-semibold leading-[22px]">
+            {isOpen ? "바꾸기" : "수정완료"}
+          </span>
+        </Button>
       </div>
     </div>
   );
